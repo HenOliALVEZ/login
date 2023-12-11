ba09baaab6c962b1501744094d91494b22df4400
@Alvez.HTML <129507411+HenOliALVEZ@users.noreply.github.com>
2023-12-11 15:21:58 -0300

    Add files via upload

---
+++ src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+// Botão de Entrar //
+
+export const Button = styled.button`
+  padding: 16px 20px;
+  outline: none;
+  border: none;
+  border-radius: 8px;
+  width: 100%;
+  cursor: pointer;
+  background-color:#FF1493;
+  color: white;
+  font-weight: 600;
+  font-size: 16px;
+  max-width: 350px;
+`;
